44a5cd98ea00a8d3c444baad503286552d4340be
Aljosa Gadjanski Bozic <aljosagadjanskibozic@gmail.com>
2025-01-11 18:05:26 +0100

    Enabled Cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Ako hocemo globalne validacije
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: true
+});
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
